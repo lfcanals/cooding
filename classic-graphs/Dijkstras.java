dcd9776261a6a68af1c1540429b260d889a816fe
lfcanals <luisfernando.canals@bbva.com>
2020-06-20 13:18:59 +0200

    Prims

---
+++ classic-graphs/Dijkstras.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+public class Dijkstras {
+  public static class WeightedVertex {
+      public final int weight;
+      public final int destiny;
+      public WeightedVertex(final int destiny, final int weight) {
+        this.weight = weight;
+        this.destiny = destiny;
+      }
+  }
+
+
+  public List<WeightedVertex>[] vertex;
+  public int numberOfNodes;
+
+
+  public Dijkstras(final int numberOfNodes) {
+    this.numberOfNodes = numberOfNodes;
+    this.vertex = new List[numberOfNodes];
+    for(int i=0; i<numberOfNodes; i++) {
+      this.vertex[i] = new ArrayList<WeightedVertex>();
+    }
+  }
+
+  
+  public int[] dijkstrasMinDistances(final int startNode) {
+    final Set<Integer> shortestPath = new HashSet<>();
+
+    final int[] distances = new int[this.numberOfNodes];
+    for(int i=0; i<distances.length; i++) distances[i] = Integer.MAX_VALUE;
+    distances[startNode] = 0;
+
+    int currentNode = startNode;
+    shortestPath.add(currentNode);
+    while(shortestPath.size()<this.numberOfNodes) {
+      for(WeightedVertex neighbour : vertex[currentNode]) {
+        final int newWeight = neighbour.weight + distances[currentNode];
+        if(newWeight<distances[neighbour.destiny]) {
+          distances[neighbour.destiny] = newWeight;
+        }
+      }
+
+      // Choose nect node:
+      // Opportunistic approach:only neighbours of used nodes can be considered
+      final Set<Integer> considered = new HashSet<Integer>();
+      int minDistance = Integer.MAX_VALUE;
+      for(Integer usedNode : shortestPath) {
+        for(WeightedVertex neighbour : vertex[usedNode]) {
+          if(shortestPath.contains(neighbour.destiny)) continue;
+          if(distances[neighbour.destiny]<minDistance) {
+            minDistance = distances[neighbour.destiny];
+            currentNode = neighbour.destiny;
+          }
+        }
+      }
+
+      for(int i=0; i<distances.length; i++) System.out.print(distances[i]+" ");
+      System.out.println();
+      System.out.println("Next node to add " + currentNode);
+      if(shortestPath.contains(currentNode)) break; // No more nodes reachable
+      shortestPath.add(currentNode);
+    }
+
+    return distances;
+  }
+
+  public void addLink(final int origin, final int destiny, final int weight) {
+    vertex[origin].add(new WeightedVertex(destiny, weight));
+  }
+
+
+  public static void main(final String args[]) throws Exception {
+    final Dijkstras graph = new Dijkstras(5);
+    graph.addLink(0,1,2);
+    graph.addLink(1,2,3);
+    graph.addLink(0,3,6);
+    graph.addLink(1,3,2);
+    graph.addLink(1,4,5);
+    graph.addLink(2,4,1);
+
+
+    final int[] distancesFromZero = graph.dijkstrasMinDistances(0);
+    for(int i=0; i<distancesFromZero.length; i++) {
+      System.out.println("Node : "+i+", dist from 0 = "+distancesFromZero[i]);
+    }
+  }
+}
---
+++ classic-graphs/Prim.java
@@ -0,0 +1,124 @@
+import java.util.*;
+
+
+public class Prim {
+  public static class Link {
+    public int from,to,weight;
+    public Link(final int from, final int to, final int weight) {
+      this.from = from;
+      this.to = to;
+      this.weight = weight;
+    }
+    public String toString() {
+      return "(" + from + "->" + to + "/" + weight + ")";
+    }
+  }
+
+  public static class Graph {
+    public List<Link>[] links;
+
+    public Graph(final int nodes) {
+      links = new List[nodes];
+      for(int i=0; i<nodes; i++) {
+        links[i] = new ArrayList<Link>();
+      }
+    }
+
+    public void addLink(final int from, final int to, final int w) {
+      links[from].add(new Link(from, to, w));
+    }
+
+    public void addLink(final Link l) {
+      if(l==null) return;
+      links[l.from].add(l);
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder("[");
+      for(int i=0; i<links.length; i++) {
+        sb.append(links[i]).append(i==links.length-1 ? "\n" : ",\n");
+      }
+      sb.append("]");
+      return sb.toString();
+    }
+  }
+  
+
+  public static class WeightedNode implements Comparable<WeightedNode>{
+    public int node;
+    public int weight;
+    public Link linkFromPrevious;
+
+    public WeightedNode(final int node) {
+      this.node = node;
+      this.linkFromPrevious = null;
+      this.weight = Integer.MAX_VALUE;
+    }
+
+    public WeightedNode(final int node, final int weight) {
+      this.node = node;
+      this.linkFromPrevious = null;
+      this.weight = weight;
+    }
+    public int compareTo(final WeightedNode other) {
+      return this.weight - other.weight;
+    }
+  }
+
+
+
+  public static Graph primMST(final Graph graph) {
+    final Graph mstGraph = new Graph(graph.links.length);
+    final Set<Integer> visited = new HashSet<>();
+    final Map<Integer, WeightedNode> aux = new HashMap<>();
+    final PriorityQueue<WeightedNode> pendingVertices = new PriorityQueue<>();
+
+    pendingVertices.add(new WeightedNode(0,0));
+    for(int i=1; i<graph.links.length; i++) {
+      final WeightedNode wn = new WeightedNode(i);
+      aux.put(i, wn);
+      pendingVertices.add(wn);
+    }
+
+    int u = 0;
+    visited.add(u);
+    while(visited.size()<graph.links.length) {
+      // wNode = node minimum weight to visit
+      final WeightedNode wNode = pendingVertices.poll();
+      visited.add(wNode.node);
+      mstGraph.addLink(wNode.linkFromPrevious);
+      for(final Link link : graph.links[wNode.node]) {
+        //if(visited.contains(link.to)) return;
+        final WeightedNode wNeighbour = aux.get(link.to);
+        final int newWeight = link.weight + wNode.weight;
+        if(wNeighbour.weight > newWeight) {
+          wNeighbour.weight = newWeight;
+          wNeighbour.linkFromPrevious = link;
+        }
+      }
+
+      // Force a reheapify
+      if(!pendingVertices.isEmpty()) {
+        pendingVertices.add(pendingVertices.remove());
+      }
+    }
+
+    return mstGraph;
+  }
+
+
+
+
+  public static void main(final String arg[]) throws Exception {
+    final Graph graph = new Graph(5);
+    graph.addLink(0,1,2);
+    graph.addLink(1,2,3);
+    graph.addLink(0,3,6);
+    graph.addLink(1,3,8);
+    graph.addLink(1,4,5);
+    graph.addLink(2,4,7);
+
+    final Graph mst = primMST(graph);
+    System.out.println(mst);
+  }
+}
