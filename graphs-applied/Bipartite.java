0342e5bdc0ab460828831ef9a8bcd4634c1c7d19
lfcanals <luisfernando.canals@bbva.com>
2020-06-20 12:22:53 +0200

    Bipartite colour

---
+++ graphs-applied/Bipartite.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+/**
+ * Detection (AND COLOURING !!) of a graph if it's bipartite, or not.
+ */
+public class Bipartite {
+  public static class Graph {
+    public List<Integer>[] links;
+    public Graph(final int numOfNodes) {
+      links = new List[numOfNodes];
+      for(int i=0; i<numOfNodes; i++) {
+        links[i] = new ArrayList<Integer>();
+      }
+    }
+
+    /**
+     * Adds bideirectional link.
+     */
+    public void addLink(final int from, final int to) {
+      links[from].add(to);
+      links[to].add(from);
+    }
+
+    public String toString() {
+      StringBuilder sb = new StringBuilder("[");
+      for(int i=0; i<links.length; i++) {
+        sb.append(links[i]).append(i==links.length-1 ? "\n" : ",\n");
+      }
+      sb.append("]");
+      return sb.toString();
+    }
+  }
+
+
+  public static boolean isBipartite(final Graph g) {
+    final int RED = 1, BLUE = -1, CLEAR = 0;
+    final int color[] = new int[g.links.length];
+
+    final Deque<Integer> list = new LinkedList<>();
+    list.add(0);
+    color[0] = RED;
+    while( ! list.isEmpty() ) {
+      final int node = list.poll();
+      for(int v : g.links[node]) {
+        if(color[v] == CLEAR) {
+          color[v] = -color[node];
+          list.add(v);
+        } else {
+          if(color[v] == color[node]) return false;
+        }
+      }
+    }
+    return true;
+  }
+
+
+  public static void main(final String args[]) {
+    Graph g = new Graph(4);
+
+    g.addLink(0,1);
+    g.addLink(0,3);
+    g.addLink(1,2);
+    g.addLink(2,3);
+
+    System.out.println(g + (isBipartite(g) ? " is":" is not") + " bipartite");
+
+    g.addLink(0,0);
+
+    System.out.println(g + (isBipartite(g) ? " is":" is not") + " bipartite");
+  }
+} 
