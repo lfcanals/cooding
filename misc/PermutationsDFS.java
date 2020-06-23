7b5d7cc7f155bf0a7f6c84aa8d2a8facb4fa1a43
lfcanals <luisfernando.canals@bbva.com>
2020-06-23 19:05:15 +0200

    Explanation to permutations using DFS

--- misc/PermutationsDFS.java
+++ misc/PermutationsDFS.java
@@ -1,5 +1,30 @@
 import java.util.*;
 
+/**
+ * Generate permutations using DFS (preorder, since it's simpler)
+ * over a N-tree.
+ *
+ * Lets see the idea for four elements: ABCD
+ * 
+ * The root is ABCD (level 0)
+ * Childs of ABCD in level 1 are swap(parent,0,i) i=0,..3
+ * Childs of ABCD in level 2 are swap(parent,1,i) i=1,..3
+ * Childs of ABCD in level 3 are swap(parent,2,i) i=2,3
+ *
+ * Childs of BACD in level 2 are swap(BACD,1,i), i=0,..3
+ * etc
+ *
+ * Mmmm.... Maybe painting the tree is simpler
+ *
+ *    ABCD
+ *     | \---------------\-------------\------------\-
+ *    ABCD              BACD           CBAD        DBCA
+ *     | \------\         \ \-----\     ...          ...
+ *    ABCD      ACBD       BACD  BCAD
+ *     |  \      |  \       | \    \ ---\         ,,,,
+ *    ABCD ABDC ACBD ACDB BACD BADC BCAD BCDA  
+ *
+ */
 public class PermutationsDFS {
   public static class NodeInTree {
     public Object[] array;
